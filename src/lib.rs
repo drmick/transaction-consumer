4c26897d549a1a1ded183734cadab706150f03be
Vladimir <v.petrzhikovskiy@dexpa.io>
2022-01-13 18:52:13 +0300

    Add separate client

--- src/lib.rs
+++ src/lib.rs
@@ -42,44 +42,81 @@ macro_rules! try_opt {
     };
 }
 
-pub struct ProducedTransaction {
-    pub id: UInt256,
-    pub transaction: ton_block::Transaction,
-    commit_channel: Option<oneshot::Sender<()>>,
+#[derive(Debug, Clone)]
+pub struct StatesClient {
+    client: reqwest::Client,
+    url: Url,
 }
 
-impl ProducedTransaction {
-    fn new(id: UInt256, transaction: ton_block::Transaction) -> (Self, oneshot::Receiver<()>) {
-        let (tx, rx) = oneshot::channel();
-        (
-            Self {
-                id,
-                transaction,
-                commit_channel: Some(tx),
-            },
-            rx,
-        )
-    }
-    pub fn commit(&mut self) -> Result<()> {
-        let committer = self.commit_channel.take().context("Already committed")?;
-        committer
-            .send(())
-            .map_err(|_| anyhow::anyhow!("Failed committing"))?;
-        Ok(())
+impl StatesClient {
+    pub fn new<U>(states_rpc_endpoint: U) -> Result<StatesClient>
+    where
+        U: AsRef<str>,
+    {
+        let client = reqwest::Client::new();
+        let states_rpc_endpoint =
+            Url::parse(states_rpc_endpoint.as_ref()).context("Bad rpc endpoint")?;
+        Ok(Self {
+            client,
+            url: states_rpc_endpoint.join("account")?,
+        })
     }
 
-    pub fn get_inner_mut(&mut self) -> (UInt256, ton_block::Transaction) {
-        (
-            std::mem::take(&mut self.id),
-            std::mem::take(&mut self.transaction),
-        )
+    pub async fn get_contract_state(
+        &self,
+        contract_address: &MsgAddressInt,
+    ) -> Result<Option<ExistingContract>> {
+        #[derive(Serialize)]
+        struct Request {
+            address: String,
+        }
+
+        let req = Request {
+            address: contract_address.to_string(),
+        };
+
+        let response = self
+            .client
+            .post(self.url.clone()) //todo improve?
+            .json(&req)
+            .send()
+            .await
+            .context("Failed sending request")?;
+
+        if let StatusCode::OK = response.status() {
+            Ok(Some(response.json().await?))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub async fn run_local(
+        &self,
+        contract_address: &MsgAddressInt,
+        function: &ton_abi::Function,
+        input: &[ton_abi::Token],
+    ) -> Result<Option<nekoton_abi::ExecutionOutput>> {
+        use nekoton_abi::FunctionExt;
+
+        let state = match self.get_contract_state(contract_address).await? {
+            Some(a) => a,
+            None => return Ok(None),
+        };
+        function
+            .clone()
+            .run_local(
+                &SimpleClock,
+                state.account,
+                &state.last_transaction_id,
+                input,
+            )
+            .map(Some)
     }
 }
 
 pub struct TransactionProducer {
     consumer: StreamConsumer,
-    states_url: Url,
-    states_client: reqwest::Client,
+    states_client: StatesClient,
     topic: String,
     subscribed: AtomicBool,
 }
@@ -100,19 +137,16 @@ impl TransactionProducer {
             .set("enable.auto.commit", "true")
             .set("auto.commit.interval.ms", "5000")
             .set("enable.auto.offset.store", "false")
-            .set("auto.offset.reset", "earliest");
+            .set("auto.offset.reset", "latest");
 
         for (k, v) in options {
             config.set(k, v);
         }
-        let states_rpc_endpoint =
-            Url::parse(states_rpc_endpoint.as_ref()).context("Bad rpc endpoint")?;
         let consumer = StreamConsumer::from_config(&config)?;
 
         Ok(Arc::new(Self {
             consumer,
-            states_url: states_rpc_endpoint.join("account").unwrap(),
-            states_client: Default::default(),
+            states_client: StatesClient::new(states_rpc_endpoint)?,
             topic: topic.to_string(),
             subscribed: AtomicBool::new(false),
         }))
@@ -156,6 +190,7 @@ impl TransactionProducer {
                     decompressor.decompress(payload),
                     "Failed decompressing block data"
                 );
+                tokio::task::yield_now().await;
                 let transaction = try_res!(
                     ton_block::Transaction::construct_from_bytes(payload_decompressed),
                     "Failed constructing block"
@@ -170,7 +205,7 @@ impl TransactionProducer {
                     return;
                 }
 
-                if let Err(_) = rx.await {
+                if rx.await.is_err() {
                     continue;
                 }
 
@@ -188,28 +223,9 @@ impl TransactionProducer {
         &self,
         contract_address: &MsgAddressInt,
     ) -> Result<Option<ExistingContract>> {
-        #[derive(Serialize)]
-        struct Request {
-            address: String,
-        }
-
-        let req = Request {
-            address: contract_address.to_string(),
-        };
-
-        let response = self
-            .states_client
-            .post(self.states_url.clone()) //todo improve?
-            .json(&req)
-            .send()
+        self.states_client
+            .get_contract_state(contract_address)
             .await
-            .context("Failed sending request")?;
-
-        if let StatusCode::OK = response.status() {
-            Ok(Some(response.json().await?))
-        } else {
-            Ok(None)
-        }
     }
 
     pub async fn run_local(
@@ -218,21 +234,47 @@ impl TransactionProducer {
         function: &ton_abi::Function,
         input: &[ton_abi::Token],
     ) -> Result<Option<nekoton_abi::ExecutionOutput>> {
-        use nekoton_abi::FunctionExt;
+        self.states_client
+            .run_local(contract_address, function, input)
+            .await
+    }
 
-        let state = match self.get_contract_state(contract_address).await? {
-            Some(a) => a,
-            None => return Ok(None),
-        };
-        function
-            .clone()
-            .run_local(
-                &SimpleClock,
-                state.account,
-                &state.last_transaction_id,
-                input,
-            )
-            .map(Some)
+    pub fn get_client(&self) -> &StatesClient {
+        &self.states_client
+    }
+}
+
+pub struct ProducedTransaction {
+    pub id: UInt256,
+    pub transaction: ton_block::Transaction,
+    commit_channel: Option<oneshot::Sender<()>>,
+}
+
+impl ProducedTransaction {
+    fn new(id: UInt256, transaction: ton_block::Transaction) -> (Self, oneshot::Receiver<()>) {
+        let (tx, rx) = oneshot::channel();
+        (
+            Self {
+                id,
+                transaction,
+                commit_channel: Some(tx),
+            },
+            rx,
+        )
+    }
+    pub fn commit(&mut self) -> Result<()> {
+        let committer = self.commit_channel.take().context("Already committed")?;
+        committer
+            .send(())
+            .map_err(|_| anyhow::anyhow!("Failed committing"))?;
+        Ok(())
+    }
+
+    pub fn get_inner_mut(&mut self) -> (UInt256, ton_block::Transaction) {
+        (
+            std::mem::take(&mut self.id),
+            std::mem::take(&mut self.transaction),
+        )
     }
 }
 
